46fda6b8f0ea1100c649e6bbeac3a2e37812028a
Christopher <slimcognitodudesvids@outlook.com>
2019-04-29 08:06:08 +1000

    Changed all the events to actual events instead of properties, made it actually asynchronous and made it so you can stop a task using a task id

--- SimpleTCPServer/Core/SimpleTCPServer.cs
+++ SimpleTCPServer/Core/SimpleTCPServer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -10,6 +11,7 @@ namespace SimpleTCPServer.Core
     /// <summary>
     /// The main class for creating a simple tcp server
     /// </summary>
+	[Serializable]
     public class TCPServer
     {
         /// <summary>
@@ -92,43 +94,82 @@ namespace SimpleTCPServer.Core
         /// The ip endpoint
         /// </summary>
         public IPEndPoint Endpoint { get => _endpoint; }
-        private IPEndPoint _endpoint;
+        private readonly IPEndPoint _endpoint;
         /// <summary>
         /// Starts everything in the server
         /// </summary>
-        public void Start()
+        public async Task StartAsync()
         {
-            Thread tcpServerRunThread = new Thread(_onStart);
-            tcpServerRunThread.Start();
+			if (isStarted)
+				return;
+
+			CancellationTokenSource src = new CancellationTokenSource();
+			_taskholders.Add(++_tasks, new TaskHolder(src, TaskType.User));
+			Task b = Task.Factory.StartNew(() => _onStart(), src.Token);
+
+			isStarted = true;
+			await b;
         }
+
+		private bool isStarted = false;
+		private long _tasks = 0;
         /// <summary>
         /// The config for the tcp server
         /// </summary>
-        public TCPServerConfig Config { get; set; } = new TCPServerConfig
+        public TCPServerConfig Config { get; private set; } = new TCPServerConfig
         {
             BytesSize = 1024,
+			KeepListOfClients = false
         };
+
+		private readonly Dictionary<long, TaskHolder> _taskholders = new Dictionary<long, TaskHolder>();
+		/// <summary>
+		/// The list of connected clients
+		/// </summary>
+		public IReadOnlyList<TcpClient> Clients
+		{
+			get
+			{
+				if (Config.KeepListOfClients)
+					return _clients;
+				return null;
+			}
+		}
+		private readonly List<TcpClient> _clients = new List<TcpClient>();
+
+		private TcpListener _listener;
         private async void _onStart()
         {
-            TcpListener server = new TcpListener(_endpoint);
-            server.Start();
-
+            _listener = new TcpListener(_endpoint);
+			_listener.Start();
+			
             await _log("Ready", "Server", LogMessageType.ServerReady);
-            await OnReady(server);
-
+            await OnReady(_listener);
+			var cancellation = _taskholders[_tasks];
             while (true)
             {
-                var client = await server.AcceptTcpClientAsync();
+                var client = await _listener.AcceptTcpClientAsync();
+				if (cancellation.TokenSource.IsCancellationRequested)
+				{
+					_listener.Stop();
+					return;
+				}
 
-                await _log("Client Connected", ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString(), LogMessageType.ClientConnected);
+				await _log("Client Connected", ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString(), LogMessageType.ClientConnected);
                 await OnClientConnect(client);
 
-                Thread TCPHandlerThread = new Thread(new ParameterizedThreadStart(_userThread));
-                TCPHandlerThread.Start(client);
-            }
+				CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+				_tasks++;
+				_taskholders.Add(_tasks, new TaskHolder(tokenSource, TaskType.Receiving));
+				Task task = Task.Factory.StartNew(() => _userThread(client, _tasks), tokenSource.Token);
+			
+				await task;
+			}
         }
        
-        private class LogMessage : ILogMessage
+		[Serializable]
+        private struct LogMessage : ILogMessage
         {
             public string Content { get; set; }
 
@@ -137,25 +178,55 @@ namespace SimpleTCPServer.Core
             public LogMessageType Type { get; set; }
         }
 
-        private async void _userThread(object sender)
+		/// <summary>
+		/// Stops a task with that taskid
+		/// </summary>
+		/// <param name="taskId">The task id</param>
+		public void StopTask(long taskId)
+		{
+			if (!_taskholders.ContainsKey(taskId))
+				return;
+			var value = _taskholders[taskId];
+			value.TokenSource.Cancel();
+			value.Dispose();
+			_taskholders.Remove(taskId);
+		}
+
+        private async void _userThread(TcpClient sender, long task)
         {
-            TcpClient mClient = (TcpClient)sender;
+            TcpClient mClient = sender;
+			_clients.Add(mClient);
+			var cancellation = _taskholders[task];
+
+			async Task cancel()
+			{
+				await _log("Client Disconnected", ((IPEndPoint)mClient.Client.RemoteEndPoint).Address.ToString(), LogMessageType.ClientDisconnected);
+				_clients.Remove(mClient);
+				await OnClientLeave(mClient);
+				_taskholders.Remove(task);
+			};
+
             while (true)
             {
                 try
                 {
-                    var stream = mClient.GetStream();
+					if (cancellation.TokenSource.IsCancellationRequested)
+					{
+						await cancel();
+						return;
+					}
+
+					var stream = mClient.GetStream();
                     byte[] bytes = new byte[Config.BytesSize];
                     await stream.ReadAsync(bytes, 0, bytes.Length);
 
-                    await _log("Bytes received", ((IPEndPoint)mClient.Client.RemoteEndPoint).Address.ToString(), LogMessageType.BytesReceived);
+					await _log("Bytes received", ((IPEndPoint)mClient.Client.RemoteEndPoint).Address.ToString(), LogMessageType.BytesReceived);
                     await BytesReceived(mClient, stream, bytes);
                 }
                 catch
                 {
-                    await _log("Client Disconnected", ((IPEndPoint)mClient.Client.RemoteEndPoint).Address.ToString(), LogMessageType.ClientDisconnected);
-                    await OnClientLeave(mClient);
-                    return;
+					await cancel();
+					return;
                 }
             }
         }
@@ -164,26 +235,32 @@ namespace SimpleTCPServer.Core
             await Log(new LogMessage { Content = content, Source = source, Type = type });
         }
 
-        /// <summary>
-        /// The log Task
-        /// </summary>
-        public Func<ILogMessage, Task> Log { set; get; } = async (ILogMessage blank) => { await Task.CompletedTask; };
+		#region Events
+
+		/// <summary>
+		/// The log Task
+		/// </summary>
+		public event Func<ILogMessage, Task> Log = async (ILogMessage blank) => { await Task.CompletedTask; };
         /// <summary>
         /// The event thats fired when the server is listening
         /// </summary>
-        public Func<TcpListener, Task> OnReady { set; get; } = async (TcpListener blank) => { await Task.CompletedTask; };
+        public event Func<TcpListener, Task> OnReady = async (TcpListener blank) => { await Task.CompletedTask; };
         /// <summary>
         /// The event thats fired when a client connects
         /// </summary>
-        public Func<TcpClient, Task> OnClientConnect { set; get; } = async (TcpClient blank) => { await Task.CompletedTask; };
+        public event Func<TcpClient, Task> OnClientConnect = async (TcpClient blank) => { await Task.CompletedTask; };
         /// <summary>
         /// The event thats fired when a client leaves
         /// </summary>
-        public Func<TcpClient, Task> OnClientLeave { set; get; } = async (TcpClient blank) => { await Task.CompletedTask; };
+        public event Func<TcpClient, Task> OnClientLeave = async (TcpClient blank) => { await Task.CompletedTask; };
         /// <summary>
         /// The event thats fired when bytes are received
         /// </summary>
-        public Func<TcpClient, NetworkStream, byte[], Task> BytesReceived { set; get; } = async (TcpClient blank1, NetworkStream blank2, byte[] blank3) => { await Task.CompletedTask; };
-    }
+        public event Func<TcpClient, NetworkStream, byte[], Task> BytesReceived = async (TcpClient blank1, NetworkStream blank2, byte[] blank3) => { await Task.CompletedTask; };
+
+		#endregion
+
+	}
+
 
 }
