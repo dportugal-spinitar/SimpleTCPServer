536374b59b5c7e4cf999fad92121ba099add5c56
Christopher <slimcognitodudesvids@outlook.com>
2019-04-29 08:08:03 +1000

    Added TaskType enum

---
+++ SimpleTCPServer/Core/TaskType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTCPServer.Core
+{
+	/// <summary>
+	/// Specifies the type of Task from the thread pool
+	/// </summary>
+	[Serializable]
+	public enum TaskType
+	{
+		/// <summary>
+		/// A task for a user
+		/// </summary>
+		User = 0b10,
+		/// <summary>
+		/// A task for receiving bytes from a user
+		/// </summary>
+		Receiving = 0b1
+	}
+}
